82d62219f0f81bb4c9198c6f9004ec405628919a
PartalogaIrina <7718118@gmail.com>
2023-04-08 14:53:52 +0300

    3464

---
+++ src/MyConponent.js
@@ -0,0 +1,11 @@
+
+export default function MyComponent() {
+    return (
+    <div className="w-4/12 h-36 my-auto mt-72 mx-auto border-2 border-purple-600 drop-shadow-2xl">
+        <h1 className="text-5xl text-center">My Component</h1>
+    </div>
+    )
+}
+
+
+ 
