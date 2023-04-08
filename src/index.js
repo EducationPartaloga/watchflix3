1ca7f03d47fb3b2679b48730537ee4651549684b
PartalogaIrina <7718118@gmail.com>
2023-04-08 15:15:16 +0300

    4564756

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MyComponent from './MyConponent';
+
 import './index.css';
+import Home from './pages/Home';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <MyComponent/>
+ <Home/>
  
 );
 
