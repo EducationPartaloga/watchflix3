f61728106990df9adc43cf5e13317d83e0162815
PartalogaIrina <7718118@gmail.com>
2023-04-22 14:58:20 +0300

    777

--- src/Components/ViewList.js
+++ src/Components/ViewList.js
@@ -1,7 +1,17 @@
 
 
-export default function ViewList() {
+export default function ViewList(props) {
+
+
+const {title, list} = props
+
     return (
-        <p>ViewList</p>
+        <div className="max-w-screen-xl mx-auto mt-40">
+            <div className="flex items-center justify-between">
+            <h2 className="text-md text-3xl">{title}</h2>
+            <button className="text-yellow-300">View More</button>
+            </div>
+       
+        </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ export default function Home() {
     return (
         <div>
             <Poster/>
-            <ViewList/>
+            <ViewList title = {"Top movies"} list = {[1,2,3,4]}/>
+            <ViewList title = {"Top TV show"} list = {[5,6,7,8]}/>
             <h1 className="flex justify-center items-center">Home</h1>
         </div>
     )
