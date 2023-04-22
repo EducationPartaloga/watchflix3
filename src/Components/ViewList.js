768d2305f97a446d7ed9fc2aa962c66b8afe69e7
PartalogaIrina <7718118@gmail.com>
2023-04-22 16:29:57 +0300

    35647

--- src/Components/ViewList.js
+++ src/Components/ViewList.js
@@ -5,21 +5,23 @@ import { useEffect } from "react"
 
 export default function ViewList(props) {
 
-    const {data, setData} = useState([])
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
-
-const {title, url} = props
+    const {title, url} = props
 
 useEffect(
     ()=>{
         fetch(url)
-        .then( Response =>  return Response.json())
+        .then( Response => Response.json())
         .then(answer => {
             setData(answer.results)
+            setIsLoading(false)
         })
     },
     []
 )
+    if (isLoading)  return(<div>Загружается...</div>)
 
     return (
         <div className="max-w-screen-xl mx-auto mt-40">
@@ -28,7 +30,7 @@ useEffect(
             <button className="text-yellow-300">View More</button>
             </div>
 
-       <ViewListCard data={{}}/>
+       <ViewListCard data={data}/>
 
         </div>
     )
