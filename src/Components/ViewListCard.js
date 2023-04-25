f685bab8af33c943f18ff1ae4de22b104121baca
PartalogaIrina <7718118@gmail.com>
2023-04-25 21:16:21 +0300

    6867

--- src/Components/ViewListCard.js
+++ src/Components/ViewListCard.js
@@ -1,11 +1,15 @@
-
+import { StarIcon, EyeIcon } from "@heroicons/react/24/outline"
 export default function ViewListCard(props) {
 
         const {title, image, genres, voteAverage, voteCount} = props
 
     
         return (
-            <div className="w-[200px] h-[300px] p-6 bg-center bg-cover" style={{backgroundImage: `url("https://image.tmdb.org/t/p/original${image}")`}}>
+            <div className="flex justify-between flex-col w-[300px] h-[400px] p-6 bg-center bg-cover" style={{backgroundImage: `url("https://image.tmdb.org/t/p/original${image}")`}}>
+                <div className="flex justify-between flex-col w-[300px] h-[400px] p-5 bg-gradient-to-b from-transparent from-60% to-black to-80%">
+
+                
+                <div className="flex">
                 {genres.map((genre)=>{
                             return ( 
                                 <span className="rounded-md bg-yellow-300 text-black py-[2px] px-2">
@@ -15,18 +19,27 @@ export default function ViewListCard(props) {
 
             })
            }
-                
+             </div>   
 
-                <div>
-                    <div>
-                        <span>{voteAverage}</span>
-                        <span>{voteCount}</span>
+
+
+                <div className="">
+                    <div className="flex gap-4 bg-yellow-300 px-2 pv-1 rounded-md">
+                        <span className="flex gap-1 text-black">
+                            <StarIcon className="h-6 w-6 text-black"/>
+                         {voteAverage}
+                         </span>
+
+                        <span className="flex gap-1 text-black">
+                             <EyeIcon className="h-6 w-6 text-black"/>
+                            {voteCount}
+                        </span>
                     </div>
                     <div>
-                     <h3>{title}</h3>
+                     <h3 className="text-2xl">{title}</h3>
                     </div>
                 </div>
-               
+                </div>
             </div>
         )
     }
