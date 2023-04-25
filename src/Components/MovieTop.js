6d7df514776203e21d715d01357608a4bbe80692
PartalogaIrina <7718118@gmail.com>
2023-04-25 20:07:29 +0300

    4575686

---
+++ src/Components/MovieTop.js
@@ -0,0 +1,73 @@
+import { useState } from "react"
+import ViewListCard from "./ViewListCard"
+import { useEffect } from "react"
+
+
+export default function ViewList(props) {
+
+    const [movies, setMovies] = useState([])
+    const [genres, setGenres] = useState([])
+    const [isLoadingMovies, setIsLoadingMovies] = useState(true)
+    const [isLoadingGenres, setisLoadingGenres] = useState(true)
+
+    const topMoviesUrl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US&page=1'
+    const genresUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US"
+
+useEffect(
+    ()=>{
+        fetch(topMoviesUrl)
+        .then( Response => Response.json())
+        .then(answer => {
+            setMovies(answer.results)
+            setIsLoadingMovies(false)
+        })
+        fetch(genresUrl)
+        .then( Response => Response.json())
+        .then(answer => {
+            setGenres(answer.genres)
+            setIsLoadingGenres(false)
+        })
+    },
+    []
+)
+    let list = []
+
+    {for (let index = 0; index < 5; index++) {
+         const movie = movies[index];
+         const newArrayGenres = []
+         movie.genres_ids.map((id)=>{
+           newArrayGenres = genres.filter((genre)=>{
+            return genre.id === id
+           })
+
+        })
+
+
+        list.push(
+             <ViewListCard 
+            title ={movie.title} 
+            image ={movie.poster_path} 
+            genres ={newArrayGenres} 
+            voteAverage ={movie.vote_average} 
+            voteCount ={movie.vote_count}
+    />
+    )
+
+}}
+
+    if (isLoadingMovies || IsLoadingGenres)  
+    return(<div>Загружается...</div>)
+
+    return (
+        <div className="max-w-screen-xl mx-auto mt-40">
+            <div className="flex items-center justify-between">
+            <h2 className="text-md text-3xl">Top Movies</h2>
+            <button className="text-yellow-300">View More</button>
+            </div>
+            {list}
+          
+       {/* <ViewListCard title ={} image ={} genres ={} voteAverage ={} voteCount = {}/> */}
+
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 
 import Poster from "../Components/Poster";
-import ViewList from "../Components/ViewList";
+import MovieTop from "../Components/MovieTop";
 
 export default function Home() {
 
 
-    const topMoviesurl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US&page=1'
+    // const topMoviesurl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US&page=1'
     const toptvShowurl = 'https://api.themoviedb.org/3/tv/top_rated?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US&page=1'
 
     return (
         <div>
             <Poster/>
-            <ViewList title = {"Top movies"} url={topMoviesurl}/>
-            <ViewList title = {"Top TV show"} url={toptvShowurl}/>
+            <MovieTop/>
+            {/* <ViewList title = {"Top movies"} url={topMoviesurl}/>
+            <ViewList title = {"Top TV show"} url={toptvShowurl}/> */}
             
         </div>
     )
